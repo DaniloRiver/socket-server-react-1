6b0630a2a59d9ee1153a44e02f78a9b02db19659
DaniloRiver <202101607@umh.edu.hn>
2024-07-20 16:12:04 -0600

    tercer commit

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 
 
@@ -23,6 +24,9 @@ class Server{
     middlewares(){
         //Desplegar directorio publico
          this.app.use( express.static(path.resolve( __dirname,'../public' )));
+         
+         //CORS
+         this.app.use( cors());
     }
 
     configurarSockets(){

35606d9d43194795b9ae3d585efc5262a5513921
DaniloRiver <202101607@umh.edu.hn>
2024-07-20 15:32:41 -0600

    primer commit

---
+++ models/server.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+
+
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Http server
+        this.server = http.createServer(this.app);
+        
+        //configuracion del socket server
+        this.io = socketio( this.server, { /*configuraciones*/ } );
+    }
+
+    middlewares(){
+        //Desplegar directorio publico
+         this.app.use( express.static(path.resolve( __dirname,'../public' )));
+    }
+
+    configurarSockets(){
+        new Sockets(this.io);
+    }
+
+    execute(){
+        //inicializar middlewares
+        this.middlewares();
+
+        //Inicializar sockets
+        this.configurarSockets();
+
+        //inicializar servers
+        this.server.listen( this.port , () =>{
+            console.log('Corriendo en el puerto:'+ this.port)
+        });
+    }
+}
+
+module.exports = Server;
